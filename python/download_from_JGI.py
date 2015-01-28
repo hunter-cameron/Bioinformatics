7753517e81cf7ae235199fdc7cf5b400b346a933
Hunter Cameron <hunter.cameron418@gmail.com>
2015-01-28 11:53:24 -0500

    feature addition in progress...

--- python/download_from_JGI.py
+++ python/download_from_JGI.py
@@ -19,15 +19,17 @@ import time
 from lxml import etree
 
 
-# TODO Add support for OID in addition to project ID
+# TODO Add support for OID in addition to project ID ---Done
         # DONE: but works by passing a (fake) user agent string to the JGI CGI. They
         # don't want bots interfacting with that (as per the "no bots 
         # allowed" message) so I don't want to use that route to access the data. 
 
-# TODO Add support for GenBank and other IMG files. -- In progress
+# TODO Add support for GenBank and other IMG files. ---Done - GBK
 
 # TODO Raise errors when issues arise rather than returning blanks. Handle these using
         # *specific* try: except statements.
+
+# TODO Naming prefix for download files by id
 
 
 # these globals are the base paths for the JGI webportals that are needed
@@ -40,17 +42,14 @@ IMG_LOOKUP = 'https://img.jgi.doe.gov/cgi-bin/w/main.cgi?'
 
 class JGIEntry(object):
 
-    def __init__(self, parent, id, id_type):
+    def __init__(self, parent, id, prefix="", convert=False):
         self.id = id
-        self.id_type = id_type
-        self.alt_id = None      # This will be used to store whichever id is not given
-                                # Ex. this is proj_id when taxon_oid is given and vice-versa
-                                # This is a first step for allowing files to be named by taxon_oid
-
-        self.downld_prefix = None
         self.parent = parent
+        self.prefix = prefix
+        self.convert = convert
         #print parent
-        
+        self.id_type = "proj"       # temp
+
         self.name = self._lookup_name()
         if not self.name:
             self.tree = None
@@ -77,39 +76,18 @@ class JGIEntry(object):
                 print("   " * level + name + "/")
                 self.print_available_files(files=files[name], level=level + 1)
         
-
     def _lookup_name(self):
         """
-        Looks up organism name based on project Id, would like to add support for IMG taxon oid, but I don't know the keyname.
+        Looks up organism name based on project id
 
-        Potential method:
-            Look up taxon id in IMG using standard link (from website and insert oid)
-            Find the download data text. This gives a lookup address identical to what I would have created using projid
         """
         
-        if self.id_type == 'img_oid':
-            # simulate header to pretend not to be bot
-            headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:24.0) Gecko/20140924 Firefox/24.0 Iceweasel/24.8.1'}
-
-            
-            my_params = {'section': 'TaxonDetail', 'page': 'taxonDetail', 'taxon_oid': self.id}
-            img_html = self.parent.session.get(OID_LOOKUP, params=my_params, headers=headers)
-            
-            m = re.search("\"genome-btn download-btn.*href=['\"](.*)['\"]", img_html.text)
-            if m:
-                print(m.group(1))
-                name_html = self.parent.session.get(m.group(1))
-            else:
-                print("Couldn't find URL for IMG id: {}".format(self.id), file=sys.stderr)
-                return ""
-
-        else:   # id_type = proj
-            my_params = {'keyName': 'jgiProjectId', 'keyValue': self.id}
-            name_html = self.parent.session.get(LOOKUP, params=my_params)    # use the parent session to maintain cookies
+        my_params = {'keyName': 'jgiProjectId', 'keyValue': self.id}
+        name_html = self.parent.session.get(LOOKUP, params=my_params)    # use the parent session to maintain cookies
         print((self.id, name_html.url))
         
         
-        # would it be possible to use the url to isolate the name? Rather than the text? I don't think it was becuase I did it this way. I think JGI may not always use the full name in their URLs.
+        # would it be possible to use the url to isolate the name? Rather than the text? I don't think it was becuase I did it this way. I think JGI may not always use the full name in all of their URLs.
 
         #print(name_html.text)
 
@@ -126,6 +104,10 @@ class JGIEntry(object):
 
             return ""
             # instead of return, might be nice to remove the reference in the parent list
+
+    def lookup_taxon_oid(self):
+        """ Coming soon! """
+        raise NotImplementedError
 
     def parse_xml(self):
         """
@@ -190,6 +172,35 @@ class JGIEntry(object):
         Wrapper for _get_download_tuple and _download_path. Allows easier user access.
 
         make_dir option will make a directory to store all download files from this instance
+        
+        Example:
+
+            prefix = 090
+            XML Structure:
+                Raw Data
+                    123.myfile.fasta
+                    456.yourfile.fasta
+
+            To download all fastas:
+
+                myobj.download_data(download_regex=
+                        [("Raw Data", "*.(yourfile.fasta)"), ("Raw Data", "*.(myfile.fasta)")],
+                        local_dir=mydir
+                        )
+
+            Results:
+                mydir/090.myfile.fasta
+                mydir/090.yourfile.fasta
+
+
+            NOTICE: Doing something like:
+                myobj.download_data(to_download=[("Raw Data", "*(file).fasta")], mydir)
+
+            will result in an error because there are two files that would be saved to 
+            the same name (090.file.fasta)
+
+            See: _get_download_tuple() for more information.
+
         """
         # if there is no tree skip 
         if not self.tree:
@@ -217,44 +228,16 @@ class JGIEntry(object):
             print("Downloading {} to\n    {}".format(url, local_dir + "/" + local_name))
             self._download_path(url, local_dir + "/" + local_name)
 
-    def _get_download_tuple(self, download_regex):
+    def _get_download_tuple(self, download_regex, concat="."):
         """
-        Given a list of files to download in a tuple form (folder, file_regex), this downloads the files and stores
-        them using the id as the prefix and and captured groups from the regex as the rest of the name. Returns a
-        tuple of (url, local_name)
+        Given download_regex = [(folder1, regex1), (folder2, regex2)]
+        this method returns = [(URL1, local_path1), (URL2), local_path2)]
 
-        Example:
+        Creates local path based on prefix + suffix where:
+            prefix = one of {self.prefix -> self.name -> self.id} in that order
+            suffix = one of {captured group 1 -> full JGI filename} in that order
 
-            Genome id = 090
-            XML Structure:
-                Raw Data
-                    123.myfile.fasta
-                    456.yourfile.fasta
-                    0.file.gbk
-
-            To download all fastas:
-
-                myobj.download_data(to_download=[("Raw Data", "*.(yourfile.fasta)"), ("Raw Data", "*.(myfile.fasta)")],
-                save_dir=mydir
-
-            Results:
-                mydir/090_myfile.fasta
-                mydir/090_yourfile.fasta
-
-
-            NOTICE: Doing something like:
-                myobj.download_data(to_download=[("Raw Data", "*.fasta")], mydir)
-
-            will result in an error because there are two files that could be downloaded.
-
-
-            This behavior, though unintuitive for people familiar with UNIX wildcards, is intentional to allow the
-            user a degree of flexibility in how files are named to make parsing though the downloaded files a bit
-            easier. Especially since, currently, JGI bundles files users may be temped to use
-            a wild card to collect into a single .tar.gz
-
-            This script is primarily expected to be used to extract a single file from multiple genomes rather than
-            many files from a few genomes.
+        Concatenates prefix and suffix together with the string specified by concat
 
             """
         
@@ -287,14 +270,26 @@ class JGIEntry(object):
                     match = re.search(regex, file_name)
                     if match:
                         url = directory[file_name]["url"]
+                        # this will be 2 step naming - prefix = org name or user supplied
+                        # - suffix = capture group or full filename
+                        # try for prefix -> organism name -> id
+                        if self.prefix:
+                            prefix = self.prefix
+                        elif self.name:
+                            prefix = self.name
+                        else:
+                            prefix = self.id
+                            
+                        # make the suffix
                         # option to name it from a captured group
                         if len(match.groups()) >= 1:
-                            local_name = self.id + "_" + match.group(1)
-                            new_files.append((url, local_name))
-                        # otherwise, I'll come up with one. It will be the name + underscore + full filename
+                            suffix = match.group(1)
                         else:
-                            new_files.append((url, self.name + "_" + file_name))
+                            suffix = file_name
 
+                        # merge the prefix and suffix with "_" to give final filename
+                        new_files.append((url, prefix + concat + suffix))
+                
                 # see if the regex was successful and add files it found to files the other regexes found
                 if new_files:
                     download_files += new_files
@@ -306,7 +301,10 @@ class JGIEntry(object):
             else:
                 #return []
                 pass
+        
         # make sure all local names are unique
+        # NOTE: This will still overwrite already existing local names, may want a "force" option
+        # to enable default to be to not overwrite files
         names = [name for url, name in download_files]
         if len(names) > len(set(names)):
             print("Attempted to download multiple files using the same local path.", file=sys.stderr)
@@ -341,9 +339,14 @@ class IMGEntry(object):
     work.
     """
 
-    def __init__(self, parent, taxon_oid):
+    DATA_TYPES = ['gbk']
+
+
+    def __init__(self, parent, taxon_oid, prefix="", convert=False):
         self.parent = parent
         self.taxon_oid = taxon_oid
+        self.prefix = prefix
+        self.convert = convert
 
         # header to display to the website
         self.header = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:24.0) Gecko/20140924 Firefox/24.0 Iceweasel/24.8.1'}
@@ -357,7 +360,7 @@ class IMGEntry(object):
         """
             
         my_params = {'section': 'TaxonDetail', 'page': 'taxonDetail', 'taxon_oid': self.taxon_oid}
-        img_html = self.parent.session.get(OID_LOOKUP, params=my_params, headers=self.header)
+        img_html = self.parent.session.get(IMG_LOOKUP, params=my_params, headers=self.header)
         
         # match the keyValue param in the link with digits to allow the keyValue param to
         # be anywhere within the link 
@@ -369,6 +372,28 @@ class IMGEntry(object):
         else:
             #print("Couldn't find URL for IMG id: {}".format(self.taxon_oid), file=sys.stderr)
             raise ValueError("Download link for found for IMG Id: {}".format(self.taxon_oid))
+
+    def print_available_files(self):
+        """ 
+        Prints available files form IMG, if convert enabled, also prints for JGI
+        Currently does no checking, just tells the file types that are implemented 
+        """
+
+        print("Files available from IMG for taxon oid {}:".format(self.taxon_oid))
+        print("\t" + "\n\t".join(self.DATA_TYPES))
+
+        if self.convert:
+            jgi = JGIEntry(id=self.lookup_proj_id(), parent=self.parent)
+            jgi.print_available_files()
+    
+
+
+    def download_data(self, download_type, resume=False, force=False):
+        if download_type in self.DATA_TYPES:
+            if download_type == 'gbk':
+                gbk_file = self.download_gbk()
+            else:
+                raise ValueError("download_type: {} is not among the acceptable types: {}".format(download_type, str(self.DATA_TYPES)))
 
     def download_genbank(self):
         """ 
@@ -422,17 +447,16 @@ class IMGEntry(object):
 
         return r.text
 
+
 class JGIInterface(object):
     """
     Class for managing the connection with JGI, responsible for logging in, establishing, and maintaining cookies
     throughout the session
     """
 
-    def __init__(self, login_file, ids, id_type):
-
+    def __init__(self, login_file):
+        self.entries = []
         self.session = self._login(login_file)
-        if ids:
-            self.genomes = self._make_genomes(ids, id_type)
 
     @staticmethod
     def _login(login_file=''):
@@ -473,37 +497,53 @@ class JGIInterface(object):
             print("Login Successful!", file=sys.stderr)
             return session
 
-    def _make_genomes(self, ids, id_type):
+    def make_entries(self, ids, id_type, prefix="", convert=False):
         """
-        Makes a JGIEntry object for each id in the id_list.
+        Makes a JGIEntry or IMGEntry object for each id in the id_list.
         """
 
+        entries = []
 
-        genomes = []
+        # get ids
+        id_list = self._read_file_or_split_arg(ids)
         
-        # TODO Change this to a try except
-        if os.path.isfile(ids):
-            #read the jgi_file for proj_ids
+        # get prefixes
+        if prefix:
+            prefix_list = self._read_file_or_split_arg(prefix)
+        else:
+            prefix_list = [""] * len(id_list)
 
-            with open(ids, 'r') as in_handle:
-                for line in in_handle:
-                    line = line.strip()
-                    proj_id = JGIEntry(self, line, id_type=id_type)
-                    genomes.append(proj_id)
+        # make all the *entry objects
+        for id, prefix in zip(id_list, prefix_list):
+            if id_type == "proj_id":
+                entry = JGIEntry(parent=self, id=id, prefix=prefix, convert=convert)
 
-        else:       # parse the ids given directly at the command line
-            ids = ids.strip()
-            elems = ids.split("|")
-            genomes = [JGIEntry(self, genome, id_type=id_type) for genome in elems]
+            elif id_type == "img_oid":
+                entry = IMGEntry(parent=self, taxon_oid=id, prefix=prefix, convert=convert)
+            else:
+                raise AssertionError("Wrong id type, should have never gotten here.")
 
+            self.entries.append(entry)
 
-        return genomes
+    @staticmethod
+    def _read_file_or_split_arg(arg, delim="|"):
+        """ 
+        Either reads the file from a filename or splits the arg
+        Returns: A list of items
+        Excepts: None
+        """
+        items = []
+        try:
+            with open(arg, 'r') as IN:
+                for line in IN:
+                    item = line.strip()
+                    items.append(item)
+        except FileNotFoundError:
+            arg = arg.strip()
+            items = arg.split(delim)
+            
+        return items
 
-        x = 1
-        for genome in self.JGI_genomes:
-            print("Downloading Genome: %s" % x)
-            x += 1
-            genome.download_data("IMG", prefix)
 
     def download(self, url):
         #download the file in chunks
@@ -517,22 +557,54 @@ class JGIInterface(object):
                     fh.write(chunk)
                     fh.flush()
 
+
 def arg_type_get(arg):
     """
     Turns a text string input from the command line to a list of touples like the program requires.
     """
+
+    # see if get is for IMG data, will check for id type later
+    if arg in IMGEntry.DATA_TYPES:
+        return arg
+
     try:
         (folder, regex) = arg.split("|", 1)
         return folder, regex
     except:
-        raise argparse.ArgumentTypeError("Error processing the --get argument.")
-
+        raise
+        #raise argparse.ArgumentTypeError("Error processing the --get argument.")
 
 def test_download_gbk(interface, img_oid):
     genome = IMGEntry(parent=interface, taxon_oid=img_oid)
 
     with open("test.gbk", 'w') as OUT:
         OUT.write(genome.download_genbank())
+
+
+def main(args):
+    """
+    Main function to manipulate the classes to download data.
+    """
+    
+    interface = JGIInterface(args.login)
+    interface.make_entries(args.ids, args.id_type, prefix=args.names, convert=args.convert)
+
+    ##### TEMP
+    #test_download_gbk(interface, args.ids)
+    #sys.exit()
+
+    # if raw download from path
+    if args.download:
+        interface.download(args.download)
+        sys.exit()
+
+    if args.get:
+        for entry in interface.entries:
+            entry.download_data(args.get, args.r)
+    else:
+        for entry in interface.entries:
+            entry.print_available_files()
+       # interface.entries[0].print_available_files()
 
 
 if __name__ == "__main__":
@@ -542,7 +614,7 @@ if __name__ == "__main__":
     prefix = os.getcwd()
 
     # is there a way to remove the whitespace at the beginning of the lines and still have it display correctly?
-    # I want this to look like this on th eoupt screen but I want it to be formatted python-style here
+    # I want this to look like this on the output screen but I want it to be formatted python-style here
 
     parser = argparse.ArgumentParser(description="""
     Author: Hunter Cameron
@@ -571,7 +643,7 @@ as a single id or a list of ids separated by '|' at the command prompt
     parser.add_argument("-id_type", help="""
 the type of id supplied
 
-    """, choices=['proj', 'img_oid'], required=True)
+    """, choices=['proj_id', 'taxon_oid'], required=True)
     parser.add_argument("--get", "-g", help="""
 folder and regex of data to download separated by '|'. More than 
 one can be included as positional arguments separated by a space. 
@@ -582,12 +654,26 @@ Ex: -g 'folder|regex' 'folder|regex'
         or gbk in the IMG Data folder.
 
 Don't supply this option to display files available for download.
+
+Optionally, files can be named using the first capturing group in the 
+regex. In the absence of a first capturing group, the 'prefix' for that
+id will be used, in absence of the 'prefix', the organism name of JGI
+will be used.
         
     """ , type=arg_type_get, nargs='*')
-    parser.add_argument("-out", help="""
-prefix for the downloads
+    parser.add_argument("--convert", "-c", help="""
+Lookup alt ids to attempt to download the specified data.
 
-    """, type=str, default=prefix)
+Ex. If ids are proj_ids and --get is "gbk", will convert each
+proj_id to taxon_oid to get the data.
+
+""", action="store_true")
+    parser.add_argument("-names", help="""
+file in the same order of 'ids' that specify a prefix for each
+id. Alternatively, can be a list on the command line separated
+by '|'
+
+    """, type=str)
     parser.add_argument("-d", "--download", help="""
 single file (as URL) to download
 
@@ -598,20 +684,5 @@ resumes file downloads, skips files that already have a local path
     """, action="store_true")
     args = parser.parse_args()
 
-    interface = JGIInterface(args.login, args.ids, args.id_type)
-
-    ##### TEMP
-    #test_download_gbk(interface, args.ids)
-    #sys.exit()
-
-
-    if args.download:
-        interface.download(args.download)
-        sys.exit()
-
-    if args.get:
-        for genome in interface.genomes:
-            genome.download_data(args.get, args.r)
-    else:
-        interface.genomes[0].print_available_files()
+    main(args)
 
